d6611da5a9b95ea9f68952cc9e63149418fac206
leejiwon6315 <heman@dunamu.dev>
2023-02-14 21:59:53 +0900

    feat: theme 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,32 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import { MENUS } from './constants/menu';
+import GlobalStyle from './styles/globalStyle';
+import theme from './styles/theme';
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        {MENUS.map(({ path, component, children }) => {
-          if (children) {
-            return children.map(({ path, component }) => (
-              <Route key={path} path={path} element={component?.()} />
-            ));
-          }
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Routes>
+          {MENUS.map(({ path, component, children }) => {
+            if (children) {
+              return children.map(({ path: _path, component }) => (
+                <Route
+                  key={_path}
+                  path={`${path}/${_path}`}
+                  element={component?.()}
+                />
+              ));
+            }
 
-          return <Route key={path} path={path} element={component?.()} />;
-        })}
-      </Routes>
-    </BrowserRouter>
+            return <Route key={path} path={path} element={component?.()} />;
+          })}
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
 
