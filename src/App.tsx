360beb3a658a35e12f9623ce588b7302395e3363
leejiwon6315 <heman@dunamu.dev>
2023-02-14 20:50:27 +0900

    feat: menu setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { MENUS } from './constants/menu';
 
 const App: React.FC = () => {
-  return <div>hello world</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        {MENUS.map(({ path, component, children }) => {
+          if (children) {
+            return children.map(({ path, component }) => (
+              <Route key={path} path={path} element={component?.()} />
+            ));
+          }
+
+          return <Route key={path} path={path} element={component?.()} />;
+        })}
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ src/domain/home/index.tsx
@@ -0,0 +1,8 @@
+import { User } from '@/@types/user';
+import React from 'react';
+
+const Home = () => {
+  return <>hello world</>;
+};
+
+export default Home;
